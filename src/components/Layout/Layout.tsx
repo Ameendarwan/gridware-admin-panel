f93bda704ef5c8f8e8ff50cf6b65d0589a2b2973
Ali Nasir <alinasir85@live.com>
2024-11-14 12:41:44 +0500

    feat: sidebar & header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,21 @@
-import React, { FC, ReactNode } from 'react'
-import Sidebar from '@app/components/Sidebar'
+import React, { FC, ReactNode } from 'react';
+import Sidebar from '@app/components/Sidebar';
+import Header from '@app/components/Header/Header';
 
 interface LayoutProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <main className="flex-1 overflow-auto bg-gray-100 p-4">{children}</main>
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-auto bg-gray-100 p-4">{children}</main>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
